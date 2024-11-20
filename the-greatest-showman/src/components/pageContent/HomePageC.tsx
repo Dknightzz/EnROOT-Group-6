9074cf66a3723ff2850d15eb2a5fcfa37b9831ed
Dknightzz <davidsuny123@gmail.com>
2024-11-20 08:33:00 +0800

    Update HomePageC.tsx

--- the-greatest-showman/src/components/pageContent/HomePageC.tsx
+++ the-greatest-showman/src/components/pageContent/HomePageC.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 const HomePageC: React.FC = () => {
   return (
     <div>HomePageC</div>
+    <div>Step right up and witness the spectacle! The Greatest Showman is here to light up your night with an unforgettable blend of music, dance, and pure entertainment. 🎪✨</div>
+    <div>🗓 Date: 27th November</div>
+    <div>📍 Location: THINK TANK 21, LEVEL 3 /t THINK TANK 26, LEVEL 6</div>
+    <div>⏰ Time: 19:00 - 21:30</div>
+    <div>Prepare to be amazed as the stage transforms into a playground of vibrant performances and high-energy games infused with the rhythm of music and dance. Whether you're a fan of breathtaking choreography or heart-pounding beats, this event promises a whirlwind of fun and excitement for all.</div>
+    <h1>Why Attend?</h1>
+    <div>Electrifying music and dance games that will get everyone on their feet.</div>
+    <div>Stunning performances designed to entertain and inspire.</div>
+    <div>A perfect evening to unwind, laugh, and create lasting memories.</div>
+    <div>Don’t miss your chance to be part of The Greatest Showman experience! Sign up now and get ready for a night of spectacular entertainment.</div>
+    <div>Let the show begin! 🎭✨</div>
   )
 }
 
